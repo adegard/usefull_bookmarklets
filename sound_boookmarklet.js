b7e95b0ff633ba4dec49949d21292718e2fc97ab
adegard <degardinarnaud@gmail.com>
2024-03-27 17:56:13 +0100

    Add files via upload

---
+++ sound_boookmarklet.js
@@ -0,0 +1,10 @@
+
+
+
+
+(function() {
+    var audio = new Audio('https://adegard.github.io/markdown-cv/media/mixkit-cooking-stopwatch-alert-1792.wav');
+    audio.addEventListener('canplay', () =>{
+        audio.play();
+    });
+})();
