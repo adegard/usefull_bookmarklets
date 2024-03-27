3f156a9c7f7a7a7d5b3aab6b800e145a9f2b8185
adegard <degardinarnaud@gmail.com>
2024-03-27 17:25:23 +0100

    Add files via upload
    Notify if new items in feed Copper
    1. open https://app.copper.com/companies/<comany number>/app#/feed
    2. activate bookmarklet

---
+++ bookmarlet_feed_notify_copper.js
@@ -0,0 +1,39 @@
+async function fetchData() {
+    const res=await fetch('https://app.copper.com/api/v1/companies/190749/feed/?activity_type_id=-1&version=2&view=slim&limit=11&offset=0', {
+					  "headers": {
+						"accept": "application/json, text/javascript",
+						"Access-Control-Allow-Origin": "*",
+						"Access-Control-Allow-Methods": "GET,POST,OPTIONS,DELETE,PUT",
+						 "mode": "no-cors",
+						  "Access-Control-Allow-Origin": "*"
+					/* 	   "Referrer-Policy": "strict-origin-when-cross-origin" */
+					},
+					  "body": null,
+					  "method": "GET"
+				}
+		)
+
+   const record =await res.json();
+	console.log(record);
+	var subject = record.logs[0].target.subject;
+	
+	if(localStorage.getItem('lastfeedCopper')==undefined){
+			localStorage.setItem('lastfeedCopper', subject);
+			alert(subject);
+		}else{
+		if(localStorage.getItem('lastfeedCopper')==subject){
+			console.log("same one");
+			}else{
+			localStorage.setItem('lastfeedCopper', subject)
+			console.log("updated");
+			alert(subject);
+			}
+	}
+}
+
+fetchData(); setInterval(fetchData, 120000);
+
+/**
+use https://caiorss.github.io/bookmarklet-maker/
+to convert it in bookmarklet-maker/
+*/
